97c98a14d43b9ab5a0cd92e3a521a5e4180365c3
ohmlaws <revancedredesign@gmail.com>
2024-12-10 20:06:55 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,30 +1,25 @@
-// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js');
-importScripts('"https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging');
+importScripts('https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging.js');
 
-// Initialize Firebase
 firebase.initializeApp({
     apiKey: "AIzaSyDTThbBrX_SThsM1d7DaxFSFbQUq61iF5Y",
-  authDomain: "notify-7a8b9.firebaseapp.com",
-  projectId: "notify-7a8b9",
-  storageBucket: "notify-7a8b9.firebasestorage.app",
-  messagingSenderId: "1073549521956",
-  appId: "1:1073549521956:web:f67cfb657d2052a31cd4b1",
-  measurementId: "G-RGVQ8TF07B"
+    authDomain: "notify-7a8b9.firebaseapp.com",
+    projectId: "notify-7a8b9",
+    storageBucket: "notify-7a8b9.firebasestorage.app",
+    messagingSenderId: "1073549521956",
+    appId: "1:1073549521956:web:f67cfb657d2052a31cd4b1",
+    measurementId: "G-RGVQ8TF07B"
 });
 
-// Initialize messaging
 const messaging = firebase.messaging();
 
-// Handle background notifications
-messaging.onBackgroundMessage(function(payload) {
-    console.log('Received background message ', payload);
-
+messaging.onBackgroundMessage(payload => {
+    console.log('Background notification received:', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
         icon: payload.notification.icon
     };
 
-    return self.registration.showNotification(notificationTitle, notificationOptions);
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
